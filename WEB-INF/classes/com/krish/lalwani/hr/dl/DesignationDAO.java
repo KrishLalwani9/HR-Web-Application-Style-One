707075df0eefa7e6eb2bd20f8b6cd16dc2e0b8d7
KrishLalwani9 <krishlalwani531@gmail.com>
2024-07-23 17:50:51 +0530

    Crated AddDesignation.html and AddDesignation.java and add new features on DesignationDAO

--- WEB-INF/classes/com/krish/lalwani/hr/dl/DesignationDAO.java
+++ WEB-INF/classes/com/krish/lalwani/hr/dl/DesignationDAO.java
@@ -3,7 +3,40 @@ import java.sql.*;
 import java.util.*;
 public class DesignationDAO
 {
-public List<DesignationDTO> getAll() throws Exception
+public void addDesignation(DesignationDTO designation) throws DAOException
+{
+try
+{
+String title=designation.getTitle();
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement=connection.prepareStatement("select (code) from designation where title=(?)");
+preparedStatement.setString(1,title);
+ResultSet resultSet=preparedStatement.executeQuery();
+while(resultSet.next())
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Designation : "+title+" exists.");
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("insert into designation (title) values (?)",Statement.RETURN_GENERATED_KEYS);
+preparedStatement.setString(1,title);
+preparedStatement.executeUpdate();
+resultSet=preparedStatement.getGeneratedKeys();
+resultSet.next();
+int code=resultSet.getInt(1);
+resultSet.close();
+preparedStatement.close();
+connection.close();
+designation.setCode(code);
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage()); //remove after testing
+}
+}
+public List<DesignationDTO> getAll() throws DAOException
 {
 List<DesignationDTO> designations;
 designations=new LinkedList<>();
@@ -30,7 +63,6 @@ statement.close();
 connection.close();
 }catch(Exception exception)
 {
-System.out.println("1*******************************************************************************");
 throw new DAOException(exception.getMessage());
 }
 return designations;
---
+++ WEB-INF/classes/com/krish/lalwani/hr/servlets/AddDesignation.java
@@ -0,0 +1,154 @@
+package com.krish.lalwani.hr.servlets;
+import com.krish.lalwani.hr.dl.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+public class AddDesignation extends HttpServlet
+{
+public void doGet(HttpServletRequest request,HttpServletResponse response)
+{
+PrintWriter pw=null;
+String title="";
+try
+{
+
+pw=response.getWriter();
+response.setContentType("text/html");
+
+title=request.getParameter("title");
+DesignationDTO designation=new DesignationDTO();
+designation.setTitle(title);
+DesignationDAO designationDAO=new DesignationDAO();
+designationDAO.addDesignation(designation);
+
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>HR Application</title>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main Continer starts here -->");
+pw.println("<div style='width:90hw;margin:40px;height:auto;border:1px solid black'>");
+pw.println("<!-- header starts here -->");
+pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+pw.println("<a href='/styleone/index.html'><img src='/styleone/images/logo.png' style='float:left'></a><div style='margin-top=30px;margin-bottom:20px;font-size:25pt'>Krish Lalwani</div>");
+pw.println("</div> <!-- header ends here -->");
+pw.println("<!-- content-section starts here -->");
+pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- left panel starts here -->");
+pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+pw.println("<b>Designations</b><br>");
+pw.println("<a href='/styleone/employeesView'>Employees</a><br><br>");
+pw.println("<a href='/styleone/index.html'>home</a>");
+pw.println("</div> <!-- left panel ends here -->");
+pw.println("<!-- right panel starts here -->");
+pw.println("<div style='height:65vh;margin-top:5px;margin-bottom:5px;margin-left:105px;margin-right:5px;padding:5px;border:1px solid black'>");
+pw.println("<h3>Notification</h3>");
+pw.println("Designation added<br>");
+pw.println("<table>");
+pw.println("<tr>");
+pw.println("<td>");
+pw.println("<form action='/styleone/AddDesignation.html'>");
+pw.println("<button type='submit' style='float:left'>Yes</button>");
+pw.println("</form>");
+pw.println("</td>");
+pw.println("<td>");
+pw.println("<form action='/styleone/designationsView'>");
+pw.println("<button type='submit'>No</button>");
+pw.println("</form>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("</table>");
+pw.println("</div> <!-- right panel ends here -->");
+pw.println("</div> <!-- conten-section ends here -->");
+pw.println("<!-- footer starts here -->");
+pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>");
+pw.println("&copy; Krish Lalwani 2050");
+pw.println("</div> <!-- footer ends here -->");
+pw.println("</div> <!-- Main Continer ends here -->");
+pw.println("</body>");
+pw.println("</html>");
+
+}catch(DAOException daoException)
+{
+System.out.println("*************** gohfahgfha");
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>HR Application</title>");
+pw.println("<script>");
+pw.println("function validateForm(frm)");
+pw.println("{");
+pw.println("var title=frm.title.value.trim();");
+pw.println("var titleErrorSection=document.getElementById('titleErrorSection');");
+pw.println("titleErrorSection.innerHTML='';");
+pw.println("if(title.length==0)");
+pw.println("{");
+pw.println("titleErrorSection.innerHTML='Designation Required';");
+pw.println("frm.title.focus();");
+pw.println("return false;");
+pw.println("}");
+pw.println("return true;");
+pw.println("}");
+pw.println("</script>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main Continer starts here -->");
+pw.println("<div style='width:90hw;margin:40px;height:auto;border:1px solid black'>");
+pw.println("<!-- header starts here -->");
+pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+pw.println("<a href='/styleone/index.html'><img src='/styleone/images/logo.png' style='float:left'></a><div style='margin-top=30px;margin-bottom:20px;font-size:25pt'>Krish Lalwani</div>");
+pw.println("</div> <!-- header ends here -->");
+pw.println("<!-- content-section starts here -->");
+pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- left panel starts here -->");
+pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+pw.println("<b>Designations</b><br>");
+pw.println("<a href='/styleone/employeesView'>Employees</a><br><br>");
+pw.println("<a href='/styleone/index.html'>Home</a>");
+pw.println("</div> <!-- left panel ends here -->");
+pw.println("<!-- right panel starts here -->");
+pw.println("<div style='height:65vh;margin-top:5px;margin-bottom:5px;margin-left:105px;margin-right:5px;padding:5px;border:1px solid black'>");
+pw.println("<h2>Add (Designation Module)</h2>");
+pw.println("<div style='color:red'>"+daoException.getMessage()+"</div>");
+//pw.println("<table>");
+//pw.println("<tr>");
+//pw.println("<td>");
+pw.println("<form action='/styleone/addDesignation' onsubmit='return validateForm(this)'>");
+pw.println("Designation"); 
+pw.println("<input type='text' id='title' name='title' maxlength='35' size='36' value='"+title+"'>");
+pw.println("<span id='titleErrorSection' style='color:red'></span><br>");
+pw.println("<button type='submit' style='float:left'>Add</button>");
+pw.println("</form>");
+//pw.println("</td>");
+//pw.println("<td>");
+pw.println("<form action='/styleone/designationsView'>");
+pw.println("<button type='submit'>Cancel</button>");
+pw.println("</form>");
+//pw.println("</td>");
+//pw.println("</tr>");
+//pw.println("</table>");
+pw.println("</div> <!-- right panel ends here -->");
+pw.println("</div> <!-- conten-section ends here -->");
+pw.println("<!-- footer starts here -->");
+pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>");
+pw.println("&copy; Krish Lalwani 2050");
+pw.println("</div> <!-- footer ends here -->");
+pw.println("</div> <!-- Main Continer ends here -->");
+pw.println("</body>");
+pw.println("</html>");
+
+}
+catch(Exception exception)
+{
+System.out.println(exception.getMessage()); //remove after testing
+}
+}
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+{
+doGet(request,response);
+}
+}

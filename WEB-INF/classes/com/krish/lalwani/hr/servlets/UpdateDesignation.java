32c7f3bacc75bf32200aab090ac1a1033ea446ee
KrishLalwani9 <krishlalwani531@gmail.com>
2024-07-24 15:27:15 +0530

    new Servlet Created Updatedesignation.java and edit modeule complited

--- WEB-INF/classes/com/krish/lalwani/hr/servlets/UpdateDesignation.java
+++ WEB-INF/classes/com/krish/lalwani/hr/servlets/UpdateDesignation.java
@@ -5,13 +5,23 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 import java.util.*;
-public class EditDesignation extends HttpServlet
+public class UpdateDesignation extends HttpServlet
 {
 public void doGet(HttpServletRequest request,HttpServletResponse response)
 {
+PrintWriter pw=null;
+String title="";
+int code=0;
 try
 {
-int code=0;
+pw=response.getWriter();
+response.setContentType("text/html");
+title=request.getParameter("title").trim();
+if(title==null)
+{
+sendBackView(response);
+return;
+}
 try
 {
 code=Integer.parseInt(request.getParameter("code"));
@@ -20,13 +30,51 @@ code=Integer.parseInt(request.getParameter("code"));
 sendBackView(response);
 return;
 }
-try
-{
-PrintWriter pw=response.getWriter();
-response.setContentType("text/html");
+DesignationDTO designation=new DesignationDTO();
+designation.setTitle(title);
+designation.setCode(code);
 DesignationDAO designationDAO=new DesignationDAO();
-DesignationDTO designation=designationDAO.getByCode(code);
-String title=designation.getTitle();
+designationDAO.updateDesignation(designation);
+
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>HR Application</title>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main Continer starts here -->");
+pw.println("<div style='width:90hw;margin:40px;height:auto;border:1px solid black'>");
+pw.println("<!-- header starts here -->");
+pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+pw.println("<a href='/styleone/index.html'><img src='/styleone/images/logo.png' style='float:left'></a><div style='margin-top=30px;margin-bottom:20px;font-size:25pt'>Krish Lalwani</div>");
+pw.println("</div> <!-- header ends here -->");
+pw.println("<!-- content-section starts here -->");
+pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- left panel starts here -->");
+pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+pw.println("<b>Designations</b><br>");
+pw.println("<a href='/styleone/employeesView'>Employees</a><br><br>");
+pw.println("<a href='/styleone/index.html'>home</a>");
+pw.println("</div> <!-- left panel ends here -->");
+pw.println("<!-- right panel starts here -->");
+pw.println("<div style='height:65vh;margin-top:5px;margin-bottom:5px;margin-left:105px;margin-right:5px;padding:5px;border:1px solid black'>");
+pw.println("<h3>Notification</h3>");
+pw.println("Designation Updated.<br>");
+pw.println("<form action='/styleone/designationsView'>");
+pw.println("<button type='submit'>Ok</button>");
+pw.println("</form>");
+pw.println("</div> <!-- right panel ends here -->");
+pw.println("</div> <!-- conten-section ends here -->");
+pw.println("<!-- footer starts here -->");
+pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>");
+pw.println("&copy; Krish Lalwani 2050");
+pw.println("</div> <!-- footer ends here -->");
+pw.println("</div> <!-- Main Continer ends here -->");
+pw.println("</body>");
+pw.println("</html>");
+}catch(DAOException daoException)
+{
 pw.println("<!DOCTYPE HTML>");
 pw.println("<html lang='en'>");
 pw.println("<head>");
@@ -70,6 +118,7 @@ pw.println("</div> <!-- left panel ends here -->");
 pw.println("<!-- right panel starts here -->");
 pw.println("<div style='height:65vh;margin-top:5px;margin-bottom:5px;margin-left:105px;margin-right:5px;padding:5px;border:1px solid black'>");
 pw.println("<h2>Designation (Edit Module)</h2>");
+pw.println("<div style='color:red'>"+daoException.getMessage()+"</div>");
 pw.println("<form method='post' action='/styleone/updateDesignation' onsubmit='return validateForm(this)'>");
 pw.println("Designation"); 
 pw.println("<input type='hidden' id='code' name='code' value='"+code+"'>");
@@ -89,12 +138,8 @@ pw.println("<form id='cancelEditingForm' action='/styleone/designationsView'>");
 pw.println("</form>");
 pw.println("</body>");
 pw.println("</html>");
-}catch(DAOException daoException)
-{
-sendBackView(response);
-return;
 }
-}catch(Exception exception)
+catch(Exception exception)
 {
 System.out.println(exception.getMessage()); //remove after testing
 }
